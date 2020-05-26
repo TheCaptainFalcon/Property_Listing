afc3452ce822940e324fb6c7f5afa8d17d2a9d47
Joseph Kim <josephkim570@gmail.com>
2020-05-26 08:16:25 -0400

    Added an errors object state which is set from axios.post .catch - Added classnames library to add in an argument in className called is-invalid from bootstrap, which gives a red outline of the input field with the error message that is set from the validator on the backend. The actual message that appears is through the invalid feedback className in bootstrap with the error response data beneath the input field

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class Register extends Component {
             email: "",
             password: "",
             password2: "",
+            errors: {},
         }
         this.HandleChange = this.HandleChange.bind(this);
         this.HandleSubmit = this.HandleSubmit.bind(this);
@@ -27,21 +29,9 @@ class Register extends Component {
 
         axios.post('http://localhost:5000/users/register', newUser)
             .then(res => console.log(res.data))
-            .catch(err => console.log(err.response.data))
-
-        
-        // fetch('http://localhost:5000/users/register', {
-        //     method: 'POST',
-        //     headers: {
-        //         Accept: "application/json",
-        //         "Content-Type": "application/json"
-        //       },
-        //     body: JSON.stringify(newUser),
-
+            .catch(err => this.setState({errors: err.response.data}))
     }
-        
-        
-
+    
     HandleChange(e) {
         this.setState({
             [e.target.name] : e.target.value
@@ -49,6 +39,8 @@ class Register extends Component {
     }
 
     render() { 
+        const errors = this.state.errors;
+
         return (  
             <div>
                 <p>Register for dev purposes</p>
@@ -56,39 +48,47 @@ class Register extends Component {
                     <label/>Name
                     <input 
                         name="name" 
+                        className={classnames({ 'is-invalid' : errors.name })}
                         type="text" 
                         placeholder="Enter your name" 
                         value={this.state.name}
                         onChange={this.HandleChange}
                         required
                     />
+                    {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                     <label/> Email
                     <input 
                         name="email" 
+                        className={classnames({ 'is-invalid' : errors.email })}
                         type="email" 
                         placeholder="Enter your email" 
                         value={this.state.email}
                         onChange={this.HandleChange}
                         required
                     />
+                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                     <label/> Password
                     <input 
                         name="password" 
+                        className={classnames({ 'is-invalid' : errors.password })}
                         type="password" 
                         placeholder="Enter a password with 6+ characters" 
                         value={this.state.password}
                         onChange={this.HandleChange}
                         required
                     />
+                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                     <label/> Confirm Password
                     <input 
-                        name="password2" 
+                        name="password2"
+                        className={classnames({ 'is-invalid' : errors.password2 })} 
                         type="password" 
                         placeholder="Reenter your password" 
                         value={this.state.password2}
                         onChange={this.HandleChange}
                         required
                     />
+                    {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
                     <input type="submit"/>
                 </form>
             </div>
