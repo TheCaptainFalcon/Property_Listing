a58d801005d88016806621c8868c42d3dc65afad
Joseph Kim <josephkim570@gmail.com>
2020-05-27 21:53:38 -0400

    Added a utils folder containing the Authorization header with bearer token used in Postman when logging in user. Also added a validation folder (same from backend) with the is-empty file for use in the authReducer of the new action type called SET_CURRENT_USER. The login action saves a token to localStorage, sets the token to localStorage and sets it to the Auth header and decodes using jwt-decoder to get user data and sets the current user - when there is no token, the auth header is deleted,

--- client/src/Actions/authActions.js
+++ client/src/Actions/authActions.js
@@ -1,13 +1,45 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (userData, history) => dispatch => {
     axios.post('http://localhost:5000/users/register', userData)
-        .then(res => history.push('/users/login')
+        .then(res => history.push('/login')
         .catch(err =>
             dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
             })
+        ));
+};
+
+export const loginUser = (userData) => dispatch => {
+    axios.post('/http:localhost:5000/users/login', userData)
+        .then(res => {
+            // save to localStorage
+            const { token } = res.data;
+            // set token to localStorage
+            localStorage.setItem('jwtToken', token);
+            // set token to Auth header ("Bearer + etc")
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // set current user
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.reponse.data
+            })
         );
 };
+
+// set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
